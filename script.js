66937daf95c221c807c690f49d8c82ef4ee98157
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-03-19 17:52:00 +0100

    add

--- script.js
+++ script.js
@@ -19,16 +19,14 @@ createApp({
       for(let i = 1; i<=10; i++){
         axios.get(this.apiUrl)
         .then((risultato)=>{
-          const nuovaEmail = risultato.data.response
+          
+          const nuovaEmail = risultato.data.response;
           console.log(nuovaEmail.data);
           console.log(random.data);
           this.randomEm.push(nuovaEmail);
       });
-    //   .catch((errore) =>{
-    //     console.log(errore);
-    //   })
-    //  },
     };
+   },
 
   },
 
